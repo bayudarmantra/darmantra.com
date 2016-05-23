32cc6f28e8db96c40498a88e69c7d8ca583ec18e
Bayu Darmantra <tokeknempel@gmail.com>
2016-05-24 06:27:02 +0800

    add sass

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,7 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
