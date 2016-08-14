bdef1f509338a09997c6f6b7c2cae0aa408cc031
bayudarmantra <tokeknempel@gmail.com>
2016-08-15 06:57:44 +0800

    file manager UI

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,9 +7,16 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  /* Bootstrap */
   app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
   app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
   app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts'
+  });
+
+  /* Font Awesome */
+  app.import(app.bowerDirectory + '/font-awesome/css/font-awesome.css');
+  app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff', {
     destDir: 'fonts'
   });
 
